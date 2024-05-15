9857c151dd3172b83b34365bcbc48be94093f9fb
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2024-05-15 08:53:30 +0700

    final code
    POS mern stack app

---
+++ utils/data.js
@@ -0,0 +1,43 @@
+const items = [
+  {
+    name: "Tea",
+    price: 1,
+    category: "drinks",
+    image:
+      "https://img.etimg.com/photo/msid-69212931,quality-100/chai-itself-was-once-a-trend-that-developed-this-way-.jpg",
+  },
+  {
+    name: "coffee",
+    price: 2,
+    category: "drinks",
+    image: "https://i.cdn.newsbytesapp.com/images/l110_14211532861690.jpg",
+  },
+  {
+    name: "Chicken Biryani",
+    price: 10,
+    category: "rice",
+    image:
+      "https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg",
+  },
+  {
+    name: "Chicken Hakka",
+    price: 7,
+    category: "noodles",
+    image: "https://cravecookclick.com/wp-content/uploads/2012/07/IMG_4400.jpg",
+  },
+  {
+    name: "Eggy",
+    price: 5,
+    category: "noodles",
+    image:
+      "https://www.licious.in/blog/wp-content/uploads/2020/12/Egg-Noodles-min.jpg",
+  },
+  {
+    name: "Noodle Soup",
+    price: 5,
+    category: "noodles",
+    image:
+      "https://www.simplyrecipes.com/thmb/HP7lfJQQ9Klk1GfVTaryaYi6Cd8=/2000x1333/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__04__Beef-Pho-LEAD-2-afc6b6a9144947fb9d72070d7ea8c95c.jpg",
+  },
+];
+module.exports = items;
